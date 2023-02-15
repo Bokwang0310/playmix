70ba52fd5f6d31728cea2711491d426e09d1bc9f
bokwang <chdl2353@naver.com>
2023-02-15 19:58:42 +0900

    Add form validation

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -9,3 +9,39 @@ export const getYoutubeVideoIdFromUrl = (url: string) =>
 
 export const getYoutubeThumbnailFromUrl = (url: string) =>
   `https://i.ytimg.com/vi/${getYoutubeVideoIdFromUrl(url)}/hqdefault.jpg`;
+
+export const validateTitle = (title: FormDataEntryValue | null) => {
+  if (!title) return "제목을 입력해 주세요";
+};
+
+// TODO: 옵셔널하게 만듭시다.
+export const validateDescription = (description: FormDataEntryValue | null) => {
+  if (!description) return "설명을 입력해 주세요";
+};
+
+const validateMusicTitle = (musicTitle: FormDataEntryValue | null) => {
+  if (!musicTitle) return "제목을 입력해 주세요";
+};
+
+export const validateMusicTitles = (musicTitles: FormDataEntryValue[]) => {
+  return musicTitles.map(
+    (musicTitle) => validateMusicTitle(musicTitle) || undefined
+  );
+};
+
+const validateUrl = (url: FormDataEntryValue | null) => {
+  if (typeof url !== "string") return "URL은 문자열이어야 합니다";
+  if (!url) return "URL을 입력해 주세요";
+  if (!validateYouTubeUrl(url)) return "유튜브 URL이 아닙니다";
+};
+
+const validateYouTubeUrl = (url: string) => {
+  const regExp =
+    /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
+  const match = url.match(regExp);
+  return match && match[2].length == 11 ? true : false;
+};
+
+export const validateUrls = (urls: FormDataEntryValue[]) => {
+  return urls.map((url) => validateUrl(url) || undefined);
+};
--- app/routes/create.tsx
+++ app/routes/create.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useActionData, Form, useSubmit } from "@remix-run/react";
+import type { ActionArgs } from "@remix-run/node";
+import { redirect, json } from "@remix-run/node";
 
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
@@ -11,9 +14,54 @@ import CloseIcon from "@mui/icons-material/Close";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 import Header from "~/components/Header";
+// import type { Playlist } from "~/lib/api";
+import { nanoid } from "nanoid";
+import {
+  validateTitle,
+  validateDescription,
+  validateMusicTitles,
+  validateUrls,
+} from "~/lib/utils";
+
+export async function action({ request }: ActionArgs) {
+  const body = await request.formData();
+
+  const title = body.get("title");
+  const description = body.get("description");
+  const musicTitles = body.getAll("musicTitle");
+  const urls = body.getAll("url");
+
+  const formErrors = {
+    title: validateTitle(title),
+    description: validateDescription(description),
+    musicTitles: validateMusicTitles(musicTitles),
+    urls: validateUrls(urls),
+  };
+
+  const data = {
+    id: nanoid(),
+    title,
+    description,
+    owner: "owner",
+    // TODO: 여기 최적화
+    musics: musicTitles.map((title, index) => ({
+      title,
+      url: urls[index],
+    })),
+  };
+
+  if (Object.values(formErrors).some(Boolean))
+    return json({ formErrors, data });
+
+  return redirect("/create");
+}
 
 export default function () {
+  // 플레이리스트에 넣으려는 음악의 개수
   const [count, setCount] = useState(3);
+  const submit = useSubmit();
+  const formRef = useRef<HTMLFormElement>(null);
+  const actionData = useActionData<typeof action>();
 
   return (
     <main>
@@ -22,40 +70,47 @@ export default function () {
         leftIcon={<CloseIcon />}
         rightIcon={<ArrowForwardIosIcon />}
         leftLink="/"
-        rightOnClick={() => {}}
+        rightOnClick={() => submit(formRef.current, { replace: true })}
       />
-      <List subheader={<ListSubheader>General Setting</ListSubheader>}>
-        <ListItem>
-          <TextField
-            id="playlist-name"
-            variant="standard"
-            label="제목"
-            fullWidth
-          />
-        </ListItem>
-        <ListItem>
-          <TextField
-            id="playlist-description"
-            variant="standard"
-            multiline
-            maxRows={4}
-            label="설명"
-            fullWidth
-          />
-        </ListItem>
-        <List
-          subheader={
-            <ListSubheader>
-              Music List
-              <IconButton onClick={() => setCount(count + 1)}>
-                <AddIcon />
-              </IconButton>
-            </ListSubheader>
-          }
-        >
-          {[...Array(count)]
-            .map((_, index) => index + 1)
-            .map((i) => (
+      <Form ref={formRef} method="post">
+        <List subheader={<ListSubheader>General Setting</ListSubheader>}>
+          <ListItem>
+            <TextField
+              error={actionData?.formErrors.title ? true : false}
+              id="playlist-name"
+              variant="standard"
+              label="제목"
+              name="title"
+              fullWidth
+              defaultValue={actionData?.data.title}
+              helperText={actionData?.formErrors.title}
+            />
+          </ListItem>
+          <ListItem>
+            <TextField
+              error={actionData?.formErrors.description ? true : false}
+              id="playlist-description"
+              variant="standard"
+              multiline
+              maxRows={4}
+              label="설명"
+              name="description"
+              fullWidth
+              defaultValue={actionData?.data.description}
+              helperText={actionData?.formErrors.description}
+            />
+          </ListItem>
+          <List
+            subheader={
+              <ListSubheader>
+                Music List
+                <IconButton onClick={() => setCount(count + 1)}>
+                  <AddIcon />
+                </IconButton>
+              </ListSubheader>
+            }
+          >
+            {[...Array(count)].map((_, i) => (
               <ListItem
                 key={i}
                 sx={{
@@ -64,21 +119,30 @@ export default function () {
                 }}
               >
                 <TextField
+                  error={actionData?.formErrors.musicTitles[i] ? true : false}
                   id={`song-name-${i}`}
                   variant="standard"
-                  label={`${i}번 곡 이름`}
+                  label={`${i + 1}번 곡 제목`}
                   sx={{ width: "128px" }}
+                  name="musicTitle"
+                  defaultValue={actionData?.data.musics[i].title}
+                  helperText={actionData?.formErrors.musicTitles[i]}
                 />
                 <TextField
+                  error={actionData?.formErrors.urls[i] ? true : false}
                   id={`song-link-${i}`}
                   variant="standard"
-                  label={`${i}번 곡 URL`}
+                  label={`${i + 1}번 곡 URL`}
                   sx={{ width: "240px" }}
+                  name="url"
+                  defaultValue={actionData?.data.musics[i].url}
+                  helperText={actionData?.formErrors.urls[i]}
                 />
               </ListItem>
             ))}
+          </List>
         </List>
-      </List>
+      </Form>
     </main>
   );
 }
